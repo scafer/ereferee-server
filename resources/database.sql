c388274743be6b6534baa8783d4d5d5364140240
Sandro Ferreira <sandroacferreira@gmail.com>
2021-04-23 14:47:47 +0100

    Update: dependencies version (#3)

--- resources/database.sql
+++ resources/database.sql
@@ -1,100 +1,211 @@
 CREATE TABLE "users" (
   "id" SERIAL PRIMARY KEY,
-  "username" varchar,
-  "password" varchar,
-  "email" varchar
+  "username" varchar UNIQUE NOT NULL,
+  "password" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "accessLevel" int DEFAULT 0,
+  "notifications" int,
+  "status" int,
+  "imageId" int,
+  "created" varchar,
+  "updated" varchar
+);
+
+CREATE TABLE "athletes" (
+  "id" SERIAL PRIMARY KEY,
+  "key" varchar UNIQUE,
+  "name" varchar,
+  "fullname" varchar,
+  "birthDate" varchar,
+  "birthPlace" varchar,
+  "citizenship" varchar,
+  "height" float,
+  "weight" float,
+  "position" varchar,
+  "positionKey" int,
+  "agent" varchar,
+  "currentInternational" varchar,
+  "status" varchar,
+  "clubId" int,
+  "imageId" int,
+  "created" varchar,
+  "updated" varchar
+);
+
+CREATE TABLE "clubs" (
+  "id" SERIAL PRIMARY KEY,
+  "key" varchar UNIQUE,
+  "name" varchar,
+  "fullname" varchar,
+  "country" varchar,
+  "founded" varchar,
+  "colors" varchar,
+  "members" numeric,
+  "stadium" varchar,
+  "address" varchar,
+  "homepage" varchar,
+  "imageId" int,
+  "created" varchar,
+  "updated" varchar
+);
+
+CREATE TABLE "competitions" (
+  "id" SERIAL PRIMARY KEY,
+  "key" varchar UNIQUE,
+  "name" varchar,
+  "edition" varchar,
+  "sportId" int,
+  "imageId" int,
+  "created" varchar,
+  "updated" varchar
+);
+
+CREATE TABLE "competitionBoards" (
+  "id" SERIAL PRIMARY KEY,
+  "position" int,
+  "played" int,
+  "won" int,
+  "drawn" int,
+  "lost" int,
+  "goalsFor" int,
+  "goalsAgainst" int,
+  "goalsDifference" int,
+  "points" int,
+  "clubId" int,
+  "competitionId" int,
+  "created" varchar,
+  "updated" varchar
+);
+
+CREATE TABLE "events" (
+  "id" SERIAL PRIMARY KEY,
+  "key" varchar UNIQUE,
+  "name" varchar,
+  "description" varchar,
+  "sportId" int,
+  "imageId" int,
+  "created" varchar,
+  "updated" varchar
+);
+
+CREATE TABLE "gameEvents" (
+  "id" SERIAL PRIMARY KEY,
+  "key" varchar UNIQUE,
+  "time" varchar,
+  "gameTime" varchar,
+  "eventDescription" varchar,
+  "gameId" int,
+  "eventId" int,
+  "athleteId" int,
+  "userId" int,
+  "created" varchar,
+  "updated" varchar
+);
+
+CREATE TABLE "gameAthletes" (
+  "id" SERIAL PRIMARY KEY,
+  "status" int,
+  "gameId" int,
+  "athleteId" int,
+  "created" varchar,
+  "updated" varchar
 );
 
 CREATE TABLE "games" (
   "id" SERIAL PRIMARY KEY,
   "timeStart" varchar,
   "timeEnd" varchar,
-  "homeScore" int,
-  "visitorScore" int,
   "homeColor" varchar,
   "visitorColor" varchar,
+  "homeScore" int,
+  "visitorScore" int,
+  "homePenaltyScore" int,
+  "visitorPenaltyScore" int,
   "status" int,
-  "creationDate" varchar,
+  "type" varchar,
+  "location" varchar,
+  "homeId" int,
+  "visitorId" int,
+  "competitionId" int,
+  "imageId" int,
   "userId" int,
-  "homeTeamId" int,
-  "visitorTeamId" int
+  "created" varchar,
+  "updated" varchar
 );
 
-CREATE TABLE "teams" (
-  "id" SERIAL PRIMARY KEY,
-  "name" varchar,
-  "color" varchar
-);
-
-CREATE TABLE "athletes" (
-  "id" SERIAL PRIMARY KEY,
-  "name" varchar,
-  "teamId" int
-);
-
-CREATE TABLE "events" (
-  "id" SERIAL PRIMARY KEY,
-  "description" varchar
-);
-
-CREATE TABLE "teamAthletes" (
-  "id" SERIAL PRIMARY KEY,
-  "name" varchar,
-  "teamId" int,
-  "athleteId" int,
-  "status" varchar,
-  "role" varchar,
-  "number" int,
-  "dayStart" varchar,
-  "dayEnd" varchar
-);
-
-CREATE TABLE "userGames" (
-  "id" SERIAL PRIMARY KEY,
-  "userId" int,
-  "gameId" int,
-  "role" int
-);
-
-CREATE TABLE "gameEvents" (
+CREATE TABLE "gameUser" (
   "id" SERIAL PRIMARY KEY,
   "userId" int,
   "gameId" int,
   "athleteId" int,
-  "time" varchar,
-  "eventDescription" varchar,
-  "reg" varchar
+  "created" varchar,
+  "updated" varchar
 );
 
-CREATE TABLE "gameAthletes" (
+CREATE TABLE "sports" (
   "id" SERIAL PRIMARY KEY,
-  "gameId" int,
-  "athleteId" int,
-  "reg" varchar
+  "name" varchar,
+  "imageId" int,
+  "created" varchar,
+  "updated" varchar
 );
 
-ALTER TABLE "games" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+CREATE TABLE "images" (
+  "id" SERIAL PRIMARY KEY,
+  "image" varchar,
+  "imageUrl" varchar,
+  "created" varchar,
+  "updated" varchar
+);
 
-ALTER TABLE "games" ADD FOREIGN KEY ("homeTeamId") REFERENCES "teams" ("id");
+ALTER TABLE "users" ADD FOREIGN KEY ("imageId") REFERENCES "images" ("id");
 
-ALTER TABLE "games" ADD FOREIGN KEY ("visitorTeamId") REFERENCES "teams" ("id");
+ALTER TABLE "athletes" ADD FOREIGN KEY ("clubId") REFERENCES "clubs" ("id");
 
-ALTER TABLE "athletes" ADD FOREIGN KEY ("teamId") REFERENCES "teams" ("id");
+ALTER TABLE "athletes" ADD FOREIGN KEY ("imageId") REFERENCES "images" ("id");
 
-ALTER TABLE "teamAthletes" ADD FOREIGN KEY ("teamId") REFERENCES "teams" ("id");
+ALTER TABLE "clubs" ADD FOREIGN KEY ("imageId") REFERENCES "images" ("id");
 
-ALTER TABLE "teamAthletes" ADD FOREIGN KEY ("athleteId") REFERENCES "athletes" ("id");
+ALTER TABLE "competitions" ADD FOREIGN KEY ("sportId") REFERENCES "sports" ("id");
 
-ALTER TABLE "userGames" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+ALTER TABLE "competitions" ADD FOREIGN KEY ("imageId") REFERENCES "images" ("id");
 
-ALTER TABLE "userGames" ADD FOREIGN KEY ("gameId") REFERENCES "games" ("id");
+ALTER TABLE "competitionBoards" ADD FOREIGN KEY ("clubId") REFERENCES "clubs" ("id");
 
-ALTER TABLE "gameEvents" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+ALTER TABLE "competitionBoards" ADD FOREIGN KEY ("competitionId") REFERENCES "competitions" ("id");
+
+ALTER TABLE "events" ADD FOREIGN KEY ("sportId") REFERENCES "sports" ("id");
+
+ALTER TABLE "events" ADD FOREIGN KEY ("imageId") REFERENCES "images" ("id");
 
 ALTER TABLE "gameEvents" ADD FOREIGN KEY ("gameId") REFERENCES "games" ("id");
 
+ALTER TABLE "gameEvents" ADD FOREIGN KEY ("eventId") REFERENCES "events" ("id");
+
 ALTER TABLE "gameEvents" ADD FOREIGN KEY ("athleteId") REFERENCES "athletes" ("id");
+
+ALTER TABLE "gameEvents" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
 
 ALTER TABLE "gameAthletes" ADD FOREIGN KEY ("gameId") REFERENCES "games" ("id");
 
 ALTER TABLE "gameAthletes" ADD FOREIGN KEY ("athleteId") REFERENCES "athletes" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("homeId") REFERENCES "clubs" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("visitorId") REFERENCES "clubs" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("competitionId") REFERENCES "competitions" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("imageId") REFERENCES "images" ("id");
+
+ALTER TABLE "games" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+
+ALTER TABLE "gameUser" ADD FOREIGN KEY ("userId") REFERENCES "users" ("id");
+
+ALTER TABLE "gameUser" ADD FOREIGN KEY ("gameId") REFERENCES "games" ("id");
+
+ALTER TABLE "gameUser" ADD FOREIGN KEY ("athleteId") REFERENCES "athletes" ("id");
+
+ALTER TABLE "sports" ADD FOREIGN KEY ("imageId") REFERENCES "images" ("id");
+
+COMMENT ON COLUMN "gameEvents"."key" IS 'timestamp$userId';
